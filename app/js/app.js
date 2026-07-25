f03da2aa25a8a7e434b0b155a8b399bcc8c798ae
Mahesh Mate <ermaheshmate@gmail.com>
2026-07-25 14:34:33 +0000

    Phase 3 - Added live build information

--- app/js/app.js
+++ app/js/app.js
@@ -1,1 +1,19 @@
 console.log("DevOps Dashboard Started");
+
+// Live Clock
+
+function updateClock() {
+
+    const now = new Date();
+
+    document.getElementById("clock").innerHTML =
+        now.toLocaleTimeString();
+
+    document.getElementById("buildDate").innerHTML =
+        now.toLocaleDateString();
+
+}
+
+setInterval(updateClock,1000);
+
+updateClock();
